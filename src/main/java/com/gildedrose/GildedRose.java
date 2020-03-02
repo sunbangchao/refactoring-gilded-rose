f442cee01085b656cb8c567078978009e2a35e97
SunBangchao <>
2020-03-02 17:27:09 +0800

    middle man

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -14,64 +14,8 @@ class GildedRose {
     }
 
     public void updateQuality() {
-        items.stream().forEach(item -> updateItem(item));
+        items.stream().forEach(Item::update);
     }
 
-    public void updateItem(Item item){
-        switch (item.getName()){
-            case "Aged Brie":
-                updateAgedBrice(item);
-                break;
-            case "Backstage passes to a TAFKAL80ETC concert":
-                updateBackstagePasses(item);
-                break;
-            case "Sulfuras, Hand of Ragnaros":
-                break;
-            default:
-                updateOthers(item);
 
-        }
-
-    }
-
-    private void updateAgedBrice(Item item){
-        if (item.getQuality() < 50) {
-            item.setQuality(item.getQuality() + 1);
-        }
-        item.setSell_in(item.getSell_in() - 1);
-        if (item.getSell_in() < 0 && item.getQuality() < 50) {
-            item.setQuality(item.getQuality() + 1);
-        }
-    }
-
-    private void updateBackstagePasses(Item item){
-        if (item.getQuality() < 50) {
-            item.setQuality(item.getQuality() + 1);
-            if (item.getSell_in() < 11) {
-                if (item.getQuality() < 50) {
-                    item.setQuality(item.getQuality() + 1);
-                }
-            }
-
-            if (item.getSell_in() < 6) {
-                if (item.getQuality() < 50) {
-                    item.setQuality(item.getQuality() + 1);
-                }
-            }
-        }
-        item.setSell_in(item.getSell_in() - 1);
-        if (item.getSell_in() < 0) {
-            item.setQuality(item.getQuality() - item.getQuality());
-        }
-    }
-
-    private void updateOthers(Item item){
-        if (item.getQuality() > 0) {
-            item.setQuality(item.getQuality() - 1);
-        }
-        item.setSell_in(item.getSell_in() - 1);
-        if (item.getSell_in() < 0 && item.getQuality() > 0) {
-            item.setQuality(item.getQuality() - 1);
-        }
-    }
 }
--- src/main/java/com/gildedrose/Item.java
+++ src/main/java/com/gildedrose/Item.java
@@ -34,6 +34,63 @@ public class Item {
         this.quality = quality;
     }
 
+    public void update(){
+        switch (this.getName()){
+            case "Aged Brie":
+                updateAgedBrice();
+                break;
+            case "Backstage passes to a TAFKAL80ETC concert":
+                updateBackstagePasses();
+                break;
+            case "Sulfuras, Hand of Ragnaros":
+                break;
+            default:
+                updateOthers();
+        }
+
+    }
+
+    private void updateAgedBrice(){
+        if (this.getQuality() < 50) {
+            this.setQuality(this.getQuality() + 1);
+        }
+        this.setSell_in(this.getSell_in() - 1);
+        if (this.getSell_in() < 0 && this.getQuality() < 50) {
+            this.setQuality(this.getQuality() + 1);
+        }
+    }
+
+    private void updateBackstagePasses(){
+        if (this.getQuality() < 50) {
+            this.setQuality(this.getQuality() + 1);
+            if (this.getSell_in() < 11) {
+                if (this.getQuality() < 50) {
+                    this.setQuality(this.getQuality() + 1);
+                }
+            }
+
+            if (this.getSell_in() < 6) {
+                if (this.getQuality() < 50) {
+                    this.setQuality(this.getQuality() + 1);
+                }
+            }
+        }
+        this.setSell_in(this.getSell_in() - 1);
+        if (this.getSell_in() < 0) {
+            this.setQuality(this.getQuality() - this.getQuality());
+        }
+    }
+
+    private void updateOthers(){
+        if (this.getQuality() > 0) {
+            this.setQuality(this.getQuality() - 1);
+        }
+        this.setSell_in(this.getSell_in() - 1);
+        if (this.getSell_in() < 0 && this.getQuality() > 0) {
+            this.setQuality(this.getQuality() - 1);
+        }
+    }
+
     @Override
     public String toString() {
         return this.name + ", " + this.sell_in + ", " + this.quality;
